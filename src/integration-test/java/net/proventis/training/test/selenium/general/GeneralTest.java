1d744854b4b44e3688d50e6b1516bdd5d4a81c3f
pkabus <p.kabus@proventis.net>
2018-09-24 14:48:58 +0200

    http -> https

--- src/integration-test/java/net/proventis/training/test/selenium/general/GeneralTest.java
+++ src/integration-test/java/net/proventis/training/test/selenium/general/GeneralTest.java
@@ -19,7 +19,7 @@ public class GeneralTest extends AbstractWebDriverTest {
 	 */
 	@Test
 	public void testDriver() {
-		this.getDriver().get("http://www.google.de");
+		this.getDriver().get("https://www.google.de");
 		// wait here
 		assertTrue(this.getDriver().getCurrentUrl().contains("google.de"));
 	}
